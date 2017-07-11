b67559bd74a2edb513267343518c96252da493b8
Manuel SORIN <manuelsorin@gmail.com>
2017-07-11 11:43:20 +0200

    Update form.php

--- web_page/form.php
+++ web_page/form.php
@@ -2,7 +2,6 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <style>.focus:focus {color: #14ff00;}</style>
         <link rel="stylesheet" href="style.css" />
         <title>Download Sentinel Data</title>
     </head>
